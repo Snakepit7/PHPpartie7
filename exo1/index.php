9e16c7ba78cdcd9ae0f94ff12ea6de68e9a86078
Snakepit7 <julienfollet71@gmail.com>
2018-11-21 15:38:10 +0100

    exos

---
+++ exo1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Exo1</title>
+  </head>
+  <body>
+      <div class="background">
+          <h1 class="text">PHP partie 7 - Exo 1 :</h1>
+      </div>
+      <form method="get" action="user.php">
+          <label for="lastName"> Nom : </label>
+          <input type="text" name="lastName" />
+          <label for="fisrtName"> Prénom : </label>
+          <input type="text" name="firstName" />
+          <input type="submit" value="Envoyer !" />
+      </form>
+      <?php
+        
+      ?>
+  </body>
+</html>
