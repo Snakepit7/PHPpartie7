9e29035ad5facd3e7c1fa23b921ee5ceff255e1d
Snakepit7 <julienfollet71@gmail.com>
2018-11-22 10:41:56 +0100

    exos

---
+++ exo6/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Exo6</title>
+  </head>
+  <body>
+      <div class="background">
+          <h1 class="text">PHP partie 7 - Exo 6 :</h1>
+      </div>
+      <!-- Création du formulaire qui renvoie vers index.php -->
+      <?php
+      //Si civi, lastName et fisrtName est rempli...
+        if(!isset($_POST['civi']) && !isset($_POST['lastName']) && !isset($_POST['firstName'])){
+      ?>
+      <form method="post" action="index.php">
+          <label for="civi"> Civilité: </label>
+          <!-- Menu déroulant -->
+          <select name="civi" size="1">
+              <option> Mr.</option>
+              <option> Mme.</option>
+              <option> Mlle.</option>
+          </select><br />
+          <label for="lastName"> Nom : </label>
+          <input type="text" name="lastName" /><br />
+          <label for="fisrtName"> Prénom : </label>
+          <input type="text" name="firstName" /><br />
+          <input type="submit" value="Envoyer !" />
+      </form>
+      <?php
+        }else{
+            //... Affiche l'echo
+            echo 'Bonjour '. $_POST['civi'] . ' ' . $_POST['lastName'] . ' ' . $_POST['firstName'];
+        }
+      ?>
+  </body>
+</html>
---
+++ exo7/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <title>Exo7</title>
+  </head>
+  <body>
+      <div class="background">
+          <h1 class="text">PHP partie 7 - Exo 7 :</h1>
+      </div>
+      <!-- Création du formulaire qui renvoie vers index.php -->
+      <?php
+      //Si civi, lastName et fisrtName est rempli...
+        if(!isset($_POST['civi']) && !isset($_POST['lastName']) && !isset($_POST['firstName']) && !isset($_POST['fileButton'])){
+      ?>
+      <form method="post" action="index.php">
+          <label for="civi"> Civilité: </label>
+          <!-- Menu déroulant -->
+          <select name="civi" size="1">
+              <option> Mr.</option>
+              <option> Mme.</option>
+              <option> Mlle.</option>
+          </select><br />
+          <label for="lastName"> Nom : </label>
+          <input type="text" name="lastName" /><br />
+          <label for="fisrtName"> Prénom : </label>
+          <input type="text" name="firstName" /><br />
+          <!-- boutton "file" pour envoyer des fichiers -->
+          <input type="file" name="fileButton" /><br />
+          <input type="submit" value="Envoyer !" />
+      </form>
+      <?php
+        }else{
+            //... Affiche l'echo
+            echo 'Bonjour '. $_POST['civi'] . ' ' . $_POST['lastName'] . ' ' . $_POST['firstName'] . ' votre fichier est ' . $_POST['fileButton'];
+        }
+      ?>
+  </body>
+</html>
